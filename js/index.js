60c792e1751195aca85bdd5f581f1692597a9e0d
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-06-14 12:01:54 +0500

    first

---
+++ js/index.js
@@ -0,0 +1,197 @@
+// 1-masala
+// let A = +prompt("ixtiyori son")
+// console.log(A > 0)
+
+// 2-masala
+// let A = +prompt("ixtiyori son")
+// console.log(A % 2 == 1)
+
+// 3-masala
+// let A = +prompt("ixtiyori son")
+// console.log(A % 2 == 0)
+
+// 4-masala
+// let A = +prompt("ixtiyori son")
+// let B = +prompt("ixtiyori son")
+// console.log(A > 2 && B <= 3)
+
+// 5-masala
+// let A = +prompt("ixtiyori son")
+// let B = +prompt("ixtiyori son")
+// console.log(A >= 0 || B < -2)
+
+// 6-masala
+// let A = +prompt("ixtiyori son")
+// let B = +prompt("ixtiyori son")
+// let C = +prompt("ixtiyori son")
+// console.log(A <= B <= C)
+
+// 7-masala
+// let A = +prompt("ixtiyori son")
+// let B = +prompt("ixtiyori son")
+// let C = +prompt("ixtiyori son")
+// console.log(A <= B <= C)
+
+// 8-masala
+// let A  = +prompt("ixtiyori son")
+// let B  = +prompt("ixtiyori son")
+// console.log(A % 2 == 1 && B % 2 == 1)
+
+// 9-masala
+// let A  = +prompt("ixtiyori son")
+// let B  = +prompt("ixtiyori son")
+// console.log(A % 2 == 1 || B % 2 == 1)
+
+// 10-masala
+// let A  = +prompt("ixtiyori son")
+// let B  = +prompt("ixtiyori son")
+// console.log(A % 2 == 1 && B % 2 == 0 || A % 2 == 0 && B % 2 == 1)
+
+// 11-masala
+// let A  = +prompt("ixtiyori son")
+// let B  = +prompt("ixtiyori son")
+// console.log(A % 2 == 1 && B % 2 == 1 || A % 2 == 0 && B % 2 == 0)
+
+// 12-masala
+// let A  = +prompt("ixtiyori son")
+// let B  = +prompt("ixtiyori son")
+// let C  = +prompt("ixtiyori son")
+// console.log(A > 0 && B > 0 && C > 0)
+
+// 13-masala
+// let A  = +prompt("ixtiyori son")
+// let B  = +prompt("ixtiyori son")
+// let C  = +prompt("ixtiyori son")
+// console.log(A > 0 || B > 0 || C > 0)
+
+// 14-masala
+// let A  = +prompt("ixtiyori son")
+// let B  = +prompt("ixtiyori son")
+// let C  = +prompt("ixtiyori son")
+// console.log(A > 0 && B < 0 && C < 0 || A < 0 && B > 0 && C < 0 ||A < 0 && B < 0 && C > 0)
+
+// 15-masala
+// let A  = +prompt("ixtiyori son")
+// let B  = +prompt("ixtiyori son")
+// let C  = +prompt("ixtiyori son")
+// console.log(A > 0 && B > 0 && C < 0 || A < 0 && B > 0 && C > 0 ||A > 0 && B < 0 && C > 0)
+
+// 16-masala
+// let num = +prompt("musbat son")
+// console.log(num > 9 && num < 100 && num % 2 == 0)
+
+// 17-masala
+// let num = +prompt("ixtiyori")
+// if (num > 0){
+//     num++
+// }
+// console.log(num)
+
+// 18-masala
+// let num = +prompt("ixtiyori")
+// if (num > 0){
+//     num +=1
+// }else{
+//     num -=2
+// }
+// console.log(num);
+
+// 19-masala
+// let num  = +prompt("ixtiyori")
+// if(num > 0) {
+//     num +=1
+// } else if(num < 0) {
+//     num -=2
+// } else{
+//     num = 10
+// }
+// console.log(num);
+
+// 20-masala
+// let A = +prompt("ixtiyori")
+// let B = +prompt("ixtiyori")
+// let C = +prompt("ixtiyori")
+// let count = 0
+// if (A > 0){
+//     count++
+// }
+// if (B > 0){
+//     count++
+// }
+// if (C > 0){
+//     count++
+// }
+// console.log(count);
+
+// 21-masala
+// let A = +prompt("ixtiyori")
+// let B = +prompt("ixtiyori")
+// let C = +prompt("ixtiyori")
+// let count = 0
+// let res = 0
+// if (A > 0){
+//     count++
+// } else{
+//     res++
+// }
+// if (B > 0){
+//     count++
+// } else{
+//     res++
+// }
+// if (C > 0){
+//     count++
+// } else{
+//     res++
+// }
+// console.log(count);
+// console.log(res);
+
+// 22-masala
+// let A = +prompt("ixtiyori")
+// let B = +prompt("ixtiyori")
+// if (A > B){
+//     console.log(A, B)
+// } else{
+//     console.log(B, A);
+// }
+
+// 25-masala
+// let A = +prompt("ixtiyori")
+// let B = +prompt("ixtiyori")
+// if (A > B){
+//     let C = A
+//     A = B 
+//     B = C
+// }
+// console.log(A, B);
+
+// 26-masala
+// let A = +prompt("ixtiyori")
+// let B = +prompt("ixtiyori")
+// if (A !== B){
+//     A = B = A + B
+// } else{
+//     A = B = 0
+// }
+// console.log(A, B);
+
+// 27-masala
+// let A = +prompt("ixtiyori")
+// let B = +prompt("ixtiyori")
+// if (A !== B){
+//     A = B = Math.max(A,B)
+// } else{
+//     A = B = 0
+// }
+
+// console.log(A, B)
+
+// 28-masala
+// let A = +prompt("ixtiyori")
+// let B = +prompt("ixtiyori")
+// let C = +prompt("ixtiyori")
+// let smallest = Math.min(A, B ,C)
+// console.log(smallest);
+
+// 29-masala
